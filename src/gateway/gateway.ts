6c77cfd84501b1130cd2734179e0d21b3e07c3e6
edwinnambaje <nambajeeedwin@gmail.com>
2023-09-11 02:20:23 +0200

    added new changes

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -2,13 +2,17 @@ import { OnModuleInit } from "@nestjs/common";
 import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server } from 'socket.io'
 
-@WebSocketGateway()
+@WebSocketGateway({
+    cors: {
+        origin: ['http://localhost:3000']
+    }
+})
 export class myGateway implements OnModuleInit {
     @WebSocketServer()
     server: Server;
 
     onModuleInit() {
-        this.server.on('connection', (socket) => {
+        this.server.on('connect', (socket) => {
             console.log(socket.id)
             console.log('connected')
         })
